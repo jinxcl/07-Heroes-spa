a1809fac288d2772318e70ad21d9d3d28bdaa684
Victor Gaete <victor.gaete.flores@gmail.com>
2025-03-03 17:59:07 -0300

    React Routes added, package and pages login , dc and marvel, also modified AppRouter and Main with Routes functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './styles.css'
 import { HeroesApp } from './HeroesApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </StrictMode>,
 )
